908d0cbf380680c505d794ed182962ea2637f3c4
Brandon <jiflytiger@163.com>
2018-12-23 09:23:32 +0800

    Update WooCommerceClient.java

--- src/main/java/io/github/eatchickendaybyday/wc4j/WooCommerceClient.java
+++ src/main/java/io/github/eatchickendaybyday/wc4j/WooCommerceClient.java
@@ -2,6 +2,9 @@ package io.github.eatchickendaybyday.wc4j;
 
 
 /**
+ * Client for accessing WooCommerce. All service calls made using this client are blocking, and will not return until the
+ * service call completes.
+ *
  * @author brandonbai
  * @since 2018/12/23
  */
