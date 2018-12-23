984e8ddc9f8a386916830f6d68f948da86394a80
jifeihu <jiflytiger@163.com>
2018-12-23 09:07:40 +0800

    init clients

---
+++ src/main/java/io/github/eatchickendaybyday/wc4j/WooCommerce.java
@@ -0,0 +1,8 @@
+package io.github.eatchickendaybyday.wc4j;
+
+/**
+ * @author brandonbai
+ * @since 2018/12/23
+ */
+public interface WooCommerce {
+}
---
+++ src/main/java/io/github/eatchickendaybyday/wc4j/WooCommerceClient.java
@@ -0,0 +1,9 @@
+package io.github.eatchickendaybyday.wc4j;
+
+
+/**
+ * @author brandonbai
+ * @since 2018/12/23
+ */
+public class WooCommerceClient {
+}
