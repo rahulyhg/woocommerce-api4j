6a420009f609891a97fe0df00848db0e69c275d3
jifeihu <jiflytiger@163.com>
2018-12-30 01:33:47 +0800

    add property

--- src/main/java/io/github/eatchickendaybyday/wc4j/model/GetCustomerRequest.java
+++ src/main/java/io/github/eatchickendaybyday/wc4j/model/GetCustomerRequest.java
@@ -9,4 +9,18 @@ package io.github.eatchickendaybyday.wc4j.model;
  * @since 2018/12/23
  */
 public class GetCustomerRequest {
+
+    private Long customerId;
+
+    public GetCustomerRequest(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
 }
--- src/main/java/io/github/eatchickendaybyday/wc4j/model/GetOrderRequest.java
+++ src/main/java/io/github/eatchickendaybyday/wc4j/model/GetOrderRequest.java
@@ -9,4 +9,18 @@ package io.github.eatchickendaybyday.wc4j.model;
  * @since 2018/12/23
  */
 public class GetOrderRequest {
+
+    private Long orderId;
+
+    public GetOrderRequest(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
 }
