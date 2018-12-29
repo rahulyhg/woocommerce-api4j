3bd1aa828aaa2af2d967a5b0e8c0bd0a11e4e33e
jifeihu <jiflytiger@163.com>
2018-12-30 01:34:00 +0800

    nothing

--- src/main/java/io/github/eatchickendaybyday/wc4j/WooCommerceAsyncClient.java
+++ src/main/java/io/github/eatchickendaybyday/wc4j/WooCommerceAsyncClient.java
@@ -3,6 +3,7 @@ package io.github.eatchickendaybyday.wc4j;
 
 import io.github.eatchickendaybyday.wc4j.callback.WooCommerceCallback;
 import io.github.eatchickendaybyday.wc4j.model.*;
+import io.github.eatchickendaybyday.wc4j.util.UriTemplate;
 
 /**
  * Client for accessing WooCommerce asynchronously.
@@ -14,17 +15,25 @@ public class WooCommerceAsyncClient implements WooCommerceAsync {
 
     public void getOrder(GetOrderRequest request, WooCommerceCallback<GetOrderResponse> callback) {
 
+        String uri = UriTemplate.getOrder(request.getOrderId());
+
     }
 
     public void listOrder(ListOrderRequest request, WooCommerceCallback<ListOrderResponse> callback) {
+
+        String uri = UriTemplate.listOrders();
 
     }
 
     public void getCustomer(GetCustomerRequest request, WooCommerceCallback<GetCustomerResponse> callback) {
 
+        String uri = UriTemplate.getCustomer(request.getCustomerId());
+
     }
 
     public void listCustomer(ListCustomerRequest request, WooCommerceCallback<ListCustomerResponse> callback) {
+
+        String uri = UriTemplate.listOrders();
 
     }
 }
--- src/main/java/io/github/eatchickendaybyday/wc4j/WooCommerceClient.java
+++ src/main/java/io/github/eatchickendaybyday/wc4j/WooCommerceClient.java
@@ -2,6 +2,7 @@ package io.github.eatchickendaybyday.wc4j;
 
 
 import io.github.eatchickendaybyday.wc4j.model.*;
+import io.github.eatchickendaybyday.wc4j.util.UriTemplate;
 
 /**
  * Client for accessing WooCommerce. All service calls made using this client are blocking, and will not return until the
@@ -13,18 +14,30 @@ import io.github.eatchickendaybyday.wc4j.model.*;
 public class WooCommerceClient implements WooCommerce {
 
     public GetOrderResponse getOrder(GetOrderRequest request) {
+
+        String uri = UriTemplate.getOrder(request.getOrderId());
+
         return null;
     }
 
     public ListOrderResponse listOrder(ListOrderRequest request) {
+
+        String uri = UriTemplate.listOrders();
+
         return null;
     }
 
     public GetCustomerResponse getCustomer(GetCustomerRequest request) {
+
+        String uri = UriTemplate.getCustomer(request.getCustomerId());
+
         return null;
     }
 
     public ListCustomerResponse listCustomer(ListCustomerRequest request) {
+
+        String uri = UriTemplate.listCustomers();
+
         return null;
     }
 }
