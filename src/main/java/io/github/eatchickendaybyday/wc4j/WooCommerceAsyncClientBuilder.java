2662327b0f685c0c903ae4cc54d0efc149bbbc6a
jifeihu <jiflytiger@163.com>
2018-12-30 01:11:01 +0800

    add build style

--- src/main/java/io/github/eatchickendaybyday/wc4j/WooCommerceAsyncClientBuilder.java
+++ src/main/java/io/github/eatchickendaybyday/wc4j/WooCommerceAsyncClientBuilder.java
@@ -1,6 +1,8 @@
 package io.github.eatchickendaybyday.wc4j;
 
 
+import io.github.eatchickendaybyday.wc4j.certificate.CertificateConfig;
+
 /**
  * Fluent builder for {@link io.github.eatchickendaybyday.wc4j.WooCommerceAsync}.
  *
@@ -8,4 +10,38 @@ package io.github.eatchickendaybyday.wc4j;
  * @since 2018/12/23
  */
 public class WooCommerceAsyncClientBuilder {
+
+    private CertificateConfig certificateConfig;
+
+    private String baseUrl;
+
+    private WooCommerceAsyncClientBuilder() {
+    }
+
+    public static WooCommerceAsyncClientBuilder standard() {
+        return new WooCommerceAsyncClientBuilder();
+    }
+
+    public WooCommerceAsyncClient build() {
+
+        return new WooCommerceAsyncClient();
+    }
+
+    public WooCommerceAsyncClientBuilder withCertificate(CertificateConfig certificateConfig) {
+        this.certificateConfig = certificateConfig;
+        return this;
+    }
+
+    public WooCommerceAsyncClientBuilder withBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+        return this;
+    }
+
+    public CertificateConfig getCertificateConfig() {
+        return certificateConfig;
+    }
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
 }
--- src/main/java/io/github/eatchickendaybyday/wc4j/WooCommerceClientBuilder.java
+++ src/main/java/io/github/eatchickendaybyday/wc4j/WooCommerceClientBuilder.java
@@ -1,6 +1,8 @@
 package io.github.eatchickendaybyday.wc4j;
 
 
+import io.github.eatchickendaybyday.wc4j.certificate.CertificateConfig;
+
 /**
  * Fluent builder for {@link io.github.eatchickendaybyday.wc4j.WooCommerce}.
  *
@@ -8,4 +10,38 @@ package io.github.eatchickendaybyday.wc4j;
  * @since 2018/12/23
  */
 public class WooCommerceClientBuilder {
+
+    private CertificateConfig certificateConfig;
+
+    private String baseUrl;
+
+    private WooCommerceClientBuilder() {
+    }
+
+    public static WooCommerceClientBuilder standard() {
+        return new WooCommerceClientBuilder();
+    }
+
+    public WooCommerceClient build() {
+
+        return new WooCommerceClient();
+    }
+
+    public WooCommerceClientBuilder withCertificate(CertificateConfig certificateConfig) {
+        this.certificateConfig = certificateConfig;
+        return this;
+    }
+
+    public WooCommerceClientBuilder withBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+        return this;
+    }
+
+    public CertificateConfig getCertificateConfig() {
+        return certificateConfig;
+    }
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
 }
